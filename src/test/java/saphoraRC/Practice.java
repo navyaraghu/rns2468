d79bd023766babc26f1f3ff0eda2862e21429c6b
DELL <DELL@DESKTOP-RKDJRII>
2023-03-21 12:52:37 +0530

    first commit...

---
+++ src/test/java/saphoraRC/Practice.java
@@ -0,0 +1,12 @@
+package saphoraRC;
+
+import org.testng.annotations.Test;
+
+public class Practice {
+
+	@Test
+	public void demo() {
+		System.out.println("welcome to maven");
+	}
+
+}
---
+++ src/test/java/saphoraRC/Practice1.java
@@ -0,0 +1,11 @@
+package saphoraRC;
+
+import org.testng.annotations.Test;
+
+public class Practice1
+{
+	@Test
+	public void demo1() {
+		System.out.println("welcome to testNG");
+	}
+}
